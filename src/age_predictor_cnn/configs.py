aa18a40f53024f74894961c0b1fb0c96d2be3330
Ofredy <orellanafredy96@gmail.com>
2023-12-23 17:46:02 -0600

    eliminated no longer needed gui configs

--- src/age_predictor_cnn/configs.py
+++ src/age_predictor_cnn/configs.py
@@ -5,23 +5,7 @@ import os
 import torch
 
 # GUI Configs
-LOGO_IMG_PATH = os.path.join("..", "misc", "cougar_ai_logo.gif")
-WINDOW_NAME = "Cougar AI Age Predictor App"
-PADDING_IN_BETWEEN_WIDGETS = 10
-CAM_WIDTH, CAM_HEIGHT = 640, 640
-BOUNDARY_BOX_COLOR = ( 200, 16, 46, 255)
-BOUNDARY_BOX_THICKNESS = 2
-BOUNDARY_BOX_DX_DY = 122
-TEXT_COLOR = "#C8102E"
-INSTRUCTION_TEXT = "Place Head Within The Boundary Box"
-BUTTON_TEXT = "Make Age Prediction!"
-BUTTON_TEXT_SIZE = 15
-BUTTON_COLOR = "#C8102E"
-BUTTON_WIDTH, BUTTON_HEIGHT = 30, 5
-PREDICT_AGE_LABEL_TEXT = "Predicted Age: "
-RESET_BUTTON_WIDTH, RESET_BUTTON_HEIGHT = 20, 2
-RESET_BUTTON_TEXT = "Make New Prediction"
-OUTPUT_PREDICTIONS_TXT_PATH = os.path.join("predicted_images", "predicted_ages_summary.txt")
+LOGO_IMG_PATH = os.path.join("misc", "cougar_ai_logo.png")
 
 # CNN CONFIGS
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'

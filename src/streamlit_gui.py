4a0db5a0007cc34999e84ec15ad2775808d8a7d7
Ofredy <orellanafredy96@gmail.com>
2023-12-23 17:05:36 -0600

    "got onnx conected to streamlit"

--- src/streamlit_gui.py
+++ src/streamlit_gui.py
@@ -1,6 +1,14 @@
+# Library imports
+import torch
+from torchvision import transforms
 import cv2
 import numpy as np
 import streamlit as st
+import onnx
+import onnxruntime
+
+# Our imports
+from age_predictor_cnn.configs import IMG_SIZE
 
 
 class AgePredictorGUI():
@@ -8,6 +16,9 @@ class AgePredictorGUI():
     def __init__(self):
 
         self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.onnx_path = 'age_predictor.onnx'
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                              std=[0.229, 0.224, 0.225])
 
     def _extract_face(self):
 
@@ -19,13 +30,49 @@ class AgePredictorGUI():
         gray_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         self.faces = self.face_cascade.detectMultiScale(gray_img, 1.1, 4)
 
+    def _preprocess_img(self):
+
+        self.img = cv2.resize(self.img, (IMG_SIZE, IMG_SIZE))
+        self.img = torch.tensor(self.img).permute(2, 0, 1)
+        self.img = self.normalize(self.img/255)
+        self.img = self.img.unsqueeze(0)
+
+        return self.img[None]
+    
     def _make_age_prediction(self):
 
+        if len(self.faces) == 0:
+
+            self.age_prediction = None
+            return
+        
         x, y, w, h = self.faces[0]
 
         self.img = self.img[ y:y+h, x:x+w ]
+
+        self._preprocess_img()   
+
+        # Loading in the model
+        age_predictor_onnx = onnx.load(self.onnx_path)
+        onnx.checker.check_model(age_predictor_onnx)
+
+        ort_session = onnxruntime.InferenceSession(self.onnx_path, providers=['CPUExecutionProvider'])
+
+        def to_numpy(tensor):
+            return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+        ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(self.img)}
+        ort_outs = ort_session.run(None, ort_inputs)
+
+        self.age_prediction = int(80*ort_outs[0][0][0])
+
+    def _display_predicted_age(self):
         
-        cv2.imwrite("test1.jpg", self.img)
+        if self.age_prediction == None:
+            st.text("Face not detected")
+        else:
+            st.text("Age Predicted: %d" % (self.age_prediction))
+
 
     def window(self):
 
@@ -39,6 +86,10 @@ class AgePredictorGUI():
 
             # Use extracted face to predict age
             self._make_age_prediction()
+
+            # Display age_prediction
+            self._display_predicted_age()
+
 
 if __name__ == "__main__":
 
